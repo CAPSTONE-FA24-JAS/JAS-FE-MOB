f994d0ceaad2f75e8b2d962e0765dfe788b2e7d9
Gia Khiem <khiemgia070602@gmail.com>
2024-09-09 18:26:01 +0700

    Past Auctions: Build UI List Past Auction

--- app/(tabs)/past-auctions.tsx
+++ app/(tabs)/past-auctions.tsx
@@ -1,12 +1,59 @@
-// past-auctions.tsx
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ScrollView, Text, Image } from "react-native";
+import PastAuctionCard from "@/components/Pages/PastAuction/PastAuctionCard";
 
 const PastAuctions = () => {
+  // Mock data for past auctions
+  const pastAuctions = [
+    {
+      id: 1,
+      name: "Fine Jewels Auction",
+      startTime: "2024-08-29T22:26:19.2129981",
+      endTime: "2024-09-1T23:26:19.2129981",
+      image: "https://thewatchclub.vn/wp-content/uploads/2022/05/4-1.jpg",
+      status: "End Auctions",
+      totalLots: 100,
+      winner: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Antique Art Auction",
+      startTime: "2024-08-29T22:26:19.2129981",
+      endTime: "2024-09-1T23:26:19.2129981",
+      image: "https://thewatchclub.vn/wp-content/uploads/2022/05/4-1.jpg",
+      status: "End Auctions",
+      totalLots: 80,
+      winner: "Jane Smith",
+    },
+    {
+      id: 3,
+      name: "Modern Paintings Auction",
+      startTime: "2024-08-29T22:26:19.2129981",
+      endTime: "2024-09-1T23:26:19.2129981",
+      image: "https://thewatchclub.vn/wp-content/uploads/2022/05/4-1.jpg",
+      status: "Live Auctions",
+      totalLots: 120,
+      winner: "Michael Johnson",
+    },
+  ];
+
   return (
-    <View>
-      <Text>Past Auctions Screen</Text>
-    </View>
+    <ScrollView>
+      <View className="p-4">
+        {pastAuctions.map((auction) => (
+          <PastAuctionCard
+            key={auction.id}
+            auctionTitle={auction.name}
+            auctionStartTime={auction.startTime}
+            auctionEndTime={auction.endTime}
+            auctionWinner={auction.winner}
+            auctionImage={auction.image}
+            auctionStatus={auction.status}
+            totalLots={auction.totalLots}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ components/Pages/PastAuction/PastAuctionCard.tsx
@@ -0,0 +1,122 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+interface PastAuctionCardProps {
+  auctionTitle: string;
+  auctionStartTime: string;
+  auctionEndTime: string;
+  auctionWinner: string;
+  auctionImage: string;
+  auctionStatus: string;
+  totalLots: number;
+}
+
+const PastAuctionCard: React.FC<PastAuctionCardProps> = ({
+  auctionTitle,
+  auctionWinner,
+  auctionImage,
+  auctionStatus,
+  auctionStartTime,
+  auctionEndTime,
+  totalLots,
+}) => {
+  // Parse the auctionStartTime string into a Date object
+  const auctionStartTimeObj = new Date(auctionStartTime);
+  const auctionEndTimeObj = new Date(auctionEndTime);
+
+  // Define arrays for days of the week and months
+  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const monthsOfYear = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+
+  // Get formatted day, date, and month for auctionStartTime
+  const startDayOfWeek = daysOfWeek[auctionStartTimeObj.getUTCDay()];
+  const startDay = auctionStartTimeObj.getUTCDate();
+  const startMonth = monthsOfYear[auctionStartTimeObj.getUTCMonth()];
+
+  // Get formatted day, date, and month for auctionEndTime
+  const endDayOfWeek = daysOfWeek[auctionEndTimeObj.getUTCDay()];
+  const endDay = auctionEndTimeObj.getUTCDate();
+  const endMonth = monthsOfYear[auctionEndTimeObj.getUTCMonth()];
+
+  console.log("====================================");
+  console.log("auctionImage", auctionImage);
+  console.log("====================================");
+
+  return (
+    <View className="mb-4 p-2 bg-white rounded-lg shadow relative">
+      <View className="relative w-full ">
+        <Image
+          source={{ uri: auctionImage }} // Replace with your logo path
+          className="w-full h-52 rounded-md"
+          resizeMode="cover"
+        />
+        <Text className="text-xl font-bold mt-2 absolute bottom-0 text-white left-0 bg-[#0090ff8a] w-full p-2">
+          {auctionTitle}
+        </Text>
+      </View>
+      <View className="flex flex-row items-center justify-center mt-2 bg-[#ffffffad] w-1/3 absolute top-0 left-2">
+        {/* Start Time */}
+        <View className="w-[45%] mx-auto">
+          <Text className="text-center text-base">{startDayOfWeek}</Text>
+          <Text className="text-center text-2xl font-bold">{startDay}</Text>
+          <Text className="text-center text-base font-semibold">
+            {startMonth}
+          </Text>
+        </View>
+        <Text className="text-xl w-[10%] font-bold">{">"}</Text>
+
+        {/* End Time */}
+        <View className="w-[45%] mx-auto">
+          <Text className="text-center text-base">{endDayOfWeek}</Text>
+          <Text className="text-center text-2xl font-bold">{endDay}</Text>
+          <Text className="text-center text-base font-semibold">
+            {endMonth}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <View className="flex flex-row items-center my-2 border-gray-200 border-b-2 pb-2">
+          <MaterialCommunityIcons
+            name="hammer"
+            size={30}
+            color="black"
+            style={{ marginRight: 10 }}
+          />
+          <Text className="text-sm w-full text-lg font-bold uppercase">
+            {" "}
+            {auctionStatus}
+          </Text>
+        </View>
+        <View className="flex-row items-center justify-between my-1">
+          <Text className="text-lg w-[90%] text-center font-semibold uppercase text-gray-600">
+            {" "}
+            {totalLots} LOTS
+          </Text>
+          <MaterialCommunityIcons
+            name="chevron-right"
+            size={30}
+            color="gray"
+            style={{ marginRight: 10 }}
+            className=" w-fit float-right"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default PastAuctionCard;

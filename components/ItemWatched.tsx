1ee42e11c63fecbec604afeadb4c93e3c8509a7e
khiemgia <khiemgia070602@gmail.com>
2024-11-15 00:55:18 +0700

    fix Ui item in list watched jewelry

--- components/ItemWatched.tsx
+++ components/ItemWatched.tsx
@@ -14,15 +14,17 @@ interface ItemWatchedProps {
 }
 
 export const ItemWatched: React.FC<ItemWatchedProps> = ({ item }) => (
-  <View className="w-1/2 p-2">
+  <View className="w-[48%] m-1 rounded-md p-2 border-2 border-gray-100 ">
     <Image
-      source={require("../assets/item.jpg")}
+      source={{ uri: item.jewelryDTO.imageJewelries[0].imageLink }}
       className="w-full h-48 mb-2 rounded-lg"
       resizeMode="cover"
     />
-    <Text className="text-xs text-gray-600">#{item.jewelryId}</Text>
+    <Text className="text-xs font-semibold text-right text-gray-600">
+      Yewelry #{item.jewelryId}
+    </Text>
     <Text className="mb-1 text-sm font-bold" numberOfLines={1}>
-      {item.jewelryDTO.title}
+      {item.jewelryDTO.name}
     </Text>
     <Text className="text-xs text-gray-600">
       {item.jewelryDTO.estimatePriceMin} - {item.jewelryDTO.estimatePriceMin}
